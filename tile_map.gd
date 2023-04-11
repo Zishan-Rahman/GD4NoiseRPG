c794b44210dc7e44f23fc3c02b6b46072c84aaec
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-11 23:48:44 +0100

    Set more useful limits and default value for tree_cap

--- tile_map.gd
+++ tile_map.gd
@@ -2,7 +2,7 @@ extends TileMap
 
 var trees: Array[Vector2i] = [Vector2i(0,1),Vector2i(1,1),Vector2i(2,1),Vector2i(5,1),Vector2i(6,1),Vector2i(4,2)]
 @onready var NoiseSprite: Sprite2D = $Sprite2D
-@export_range(0.0, 1.0) var tree_cap: float = 0.2 
+@export_range(-1.0, 1.0) var tree_cap: float = 0.0
 var x_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_width") / tile_set.tile_size.x
 var y_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_height") / tile_set.tile_size.y
 
@@ -10,6 +10,6 @@ var y_tile_range: int = ProjectSettings.get_setting("display/window/size/viewpor
 func _ready():
 	for x in range(x_tile_range):
 		for y in range(y_tile_range):
-			var noise_point = NoiseSprite.texture.noise.get_noise_2d(x * tile_set.tile_size.x, y * tile_set.tile_size.y)
+			var noise_point: float = NoiseSprite.texture.noise.get_noise_2d(x * tile_set.tile_size.x, y * tile_set.tile_size.y)
 			if noise_point < tree_cap:
 				set_cell(0, Vector2i(x, y), 0, trees.pick_random())
