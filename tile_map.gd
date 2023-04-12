13137126975081e397e44ad2f17c02ee5eee50b5
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-12 18:32:56 +0100

    Add buildings to world generation

--- tile_map.gd
+++ tile_map.gd
@@ -1,8 +1,54 @@
 extends TileMap
 
-var trees: Array[Vector2i] = [Vector2i(0,1),Vector2i(1,1),Vector2i(2,1),Vector2i(5,1),Vector2i(6,1),Vector2i(4,2)]
+var buildings: Array[Vector2i] = [
+	Vector2i(0, 19),
+	Vector2i(1, 19),
+	Vector2i(2, 19),
+	Vector2i(3, 19),
+	Vector2i(4, 19),
+	Vector2i(5, 19),
+	Vector2i(6, 19),
+	Vector2i(7, 19),
+	Vector2i(8, 20),
+	Vector2i(0, 20),
+	Vector2i(1, 20),
+	Vector2i(2, 20),
+	Vector2i(3, 20),
+	Vector2i(4, 20),
+	Vector2i(5, 20),
+	Vector2i(6, 20),
+	Vector2i(7, 20),
+	Vector2i(8, 20),
+	Vector2i(0, 21),
+	Vector2i(1, 21),
+	Vector2i(2, 21),
+	Vector2i(3, 21),
+	Vector2i(4, 21),
+	Vector2i(5, 21),
+	Vector2i(6, 21),
+	Vector2i(7, 21),
+	Vector2i(8, 21)
+]
+var trees: Array[Vector2i] = [
+	Vector2i(0,1),
+	Vector2i(1,1),
+	Vector2i(2,1),
+	Vector2i(3,1),
+	Vector2i(4,1),
+	Vector2i(5,1),
+	Vector2i(6,1),
+	Vector2i(7,1),
+	Vector2i(0,2),
+	Vector2i(1,2),
+	Vector2i(2,2),
+	Vector2i(3,2),
+	Vector2i(4,2)
+]
+
 @onready var NoiseSprite: Sprite2D = $Sprite2D
-@export_range(-1.0, 1.0) var tree_cap: float = 0.0
+@export_range(-1.0, 1.0) var tree_cap: float = -0.048
+@export_range(-1.0, 1.0) var building_cap: float = -0.252
+@export_range(0.0, 0.5) var building_overtakes_tree: float = 0.12
 var x_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_width") / tile_set.tile_size.x
 var y_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_height") / tile_set.tile_size.y
 
@@ -11,5 +57,7 @@ func _ready():
 	for x in range(x_tile_range):
 		for y in range(y_tile_range):
 			var noise_point: float = NoiseSprite.texture.noise.get_noise_2d(x * tile_set.tile_size.x, y * tile_set.tile_size.y)
-			if noise_point < tree_cap:
+			if noise_point < tree_cap and not get_used_cells(0).has(Vector2i(x, y)):
 				set_cell(0, Vector2i(x, y), 0, trees.pick_random())
+			if ((building_cap <= tree_cap and randf() < building_overtakes_tree) or (building_cap > tree_cap and noise_point < building_cap)) and not get_used_cells(0).has(Vector2i(x, y)):
+				set_cell(0, Vector2i(x, y), 0, buildings.pick_random())
