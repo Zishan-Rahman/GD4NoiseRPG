ff02b1b5737ac77592b2b9acc405173518fab092
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-13 15:13:58 +0100

    Add player to game
    TODO: Slow down movement (somehow)

--- tile_map.gd
+++ tile_map.gd
@@ -44,6 +44,8 @@ var trees: Array[Vector2i] = [
 	Vector2i(3,2),
 	Vector2i(4,2)
 ]
+const PLAYER_SPRITE: Vector2i = Vector2i(24, 7)
+var player_placement_cell: Vector2i
 
 @onready var NoiseSprite: Sprite2D = $Sprite2D
 @export_range(-1.0, 1.0) var tree_cap: float = -0.048
@@ -58,6 +60,31 @@ func _ready():
 	paint_tiles()
 	var new_time: float = Time.get_ticks_msec() - start_time
 	print("Time taken: " + str(new_time) + "ms")
+	place_player()
+
+func _get_player_placement_cell() -> Vector2i:
+	return Vector2i(randi() % x_tile_range, randi() % y_tile_range)
+
+func place_player():
+	while get_used_cells(0).has(player_placement_cell):
+		player_placement_cell = _get_player_placement_cell()
+	set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
+
+func _is_not_out_of_bounds(cell: Vector2i) -> bool:
+	return cell.x >= 0 and cell.x < x_tile_range and cell.y >= 0 and cell.y < y_tile_range
+
+func _physics_process(_delta):
+	var previous_cell: Vector2i = player_placement_cell
+	var direction: Vector2i = Vector2i.ZERO
+	if Input.is_action_pressed("ui_up"): direction = Vector2i.UP
+	elif Input.is_action_pressed("ui_down"): direction = Vector2i.DOWN
+	elif Input.is_action_pressed("ui_left"): direction = Vector2i.LEFT
+	elif Input.is_action_pressed("ui_right"): direction = Vector2i.RIGHT
+	var new_placement_cell: Vector2i = player_placement_cell + direction
+	if (not get_used_cells(0).has(new_placement_cell) or trees.has(get_cell_atlas_coords(0, new_placement_cell))) and _is_not_out_of_bounds(new_placement_cell):
+		player_placement_cell = new_placement_cell
+		set_cell(0, previous_cell, 0)
+		set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
 
 func paint_tiles():
 	for x in range(x_tile_range):
